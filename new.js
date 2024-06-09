5b5b1a633c02195787bf0c73607bccb4a643e75c
Erum Sikander <mrsikanderghouri@gmail.com>
2024-06-09 12:52:10 +0500

    done type error repo

---
+++ new.js
@@ -0,0 +1,8 @@
+"use strict";
+// Sir-Zia-repo-03   practice:
+// type_error define:
+Object.defineProperty(exports, "__esModule", { value: true });
+var message = "Hello World";
+console.log(message);
+// var message : string = "Hello Worl";
+// console.loger(messgae);
---
+++ new.ts
@@ -0,0 +1,11 @@
+
+
+// Sir-Zia-repo-03   practice:
+// type_error define:
+
+var message : string = "Hello World";
+console.log(message);
+
+// var message : string = "Hello Worl";
+// console.loger(messgae);
+
